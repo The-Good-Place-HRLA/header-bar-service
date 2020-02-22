f965a1730a7f332ba68e539b1b10608cd15e6ef1
Edgar Barrientos <edgarbarrientos@Edgars-MacBook-Pro.local>
2020-02-22 10:12:39 -0800

    local

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -56,7 +56,7 @@ class SearchBar extends React.Component {
     if(!searchTerm.match(check)){
       return;
     }
-    axios.post('http://localhost:3001/api', { "term": searchTerm } )
+    axios.post('http://kkkkkkkkkkk:3001/api', { "term": searchTerm } )
     .then(data => {
       var results = [];
       var products = [];
